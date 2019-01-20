dd1cec0b23dfea2a792b6113cf0a9d333ec61eb8
benyoo5222 <benyoo5222@gmail.com>
2019-01-19 21:09:25 -0500

    cleaning up code

--- device.js
+++ device.js
@@ -1,15 +1,13 @@
-const constraints = {
-  audio: true,
-  video: false
-};
-
-var AudioContext = window.AudioContext || window.webkitAudioContext;
-var audioCtx = new AudioContext();
-
 window.onload = function () {
+  //Initialize speech recognition API
   var recognition = new webkitSpeechRecognition();
+
+  //When there is a voice input, "event" will have a transcription
+  //of the speech input
   recognition.onresult = function(event) {
-    console.log(event)
-  }
+    console.log(event);
+  };
+
+  //start speech recognition
   recognition.start();
 }a
